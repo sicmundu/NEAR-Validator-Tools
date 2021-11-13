f2d0bd72bf8706027d71006a32c789802f5bd580
grodstrike <grodstrike@gmail.com>
2021-11-13 21:51:25 +0400

    v0.3

--- ping_install.sh
+++ ping_install.sh
@@ -43,6 +43,47 @@ networkSelect()
 
 }
 
+cronTab()
+{
+cat >> $homedir/ping3.sh << EOF
+#!/bin/bash
+export NEAR_ENV=$NETWORK
+near call $p$POOL ping '{}' --accountId $a$ACCOUNT --gas=300000000000000
+EOF
+
+  chmod +x $homedir/ping.sh
+
+  # Adding to Cron
+  crontab -l > mycron
+  # echo new cron into cron file
+  echo "0 */1 * * * $homedir/ping.sh >> $homedir/near.log 2>&1" >> mycron
+  # install new cron file
+  crontab mycron
+  rm -f mycron
+}
+
+cronSelect()
+{
+  PS3="Enter a number: "
+  options=("CronCat" "Crontab")
+  select opt in "${options[@]}"
+  do
+    case $opt in
+       "CronCat")
+        near call manager_v1.croncat.guildnet create_task '{"contract_id": "'$p$POOL'","function_id": "ping","cadence": "0 0 * * * *","recurring": true,"deposit": "0","gas": 9000000000000}' --accountId $a$ACCOUNT --amount 10
+        echo -e "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n @@@@@@@@(((((@@@@@(((((((((((((((((((((((((((((((((((((((((((((@@@@@(((((&&#((@@\n @@@@@@&@((((((((((@@%(((((((((((((((((((((((((((((((((((((((@@@((((((((((&&#((@@\n @@@@@@@@&&@@@@@     @@@@@%(((((((((((((((((((((((((((((&&&@@(((((@@@@@&&&&&#((@@\n @@@@@@&&%%@@@@@@@@     ..@@@(((((((((((((((((((((((((&&((((((((@@@@@@@%%%&&#((@@\n @@@@@&&&..&&&@@@@@@@     ...@@@@@@@@@@@@@@@@@@@@@@@@@(((((((@@@@@@@@@@(((&&#((@@\n @@@@@&&&  ###&&@@@@@&&&       ........((((((((((((((((((((@@@@@@@@@@%%(((&&#((@@\n @@@@@&&&     ..,,,//###,.             ((((((((((((((((((((%%###((((((((((%%#((@@\n @@@@@&&&            ...               (((((((((((((((((((((((((((((((((((%%#((@@\n @@@@@&&&                              (((((((((((((((((((((((((((((((((((%%#((@@\n @@@@@&&&                                (((((((((((((((((((((((((((((((((%%#((@@\n @@@@@&&&                                (((((((((((((((((((((((((((((((((##(((@@\n @@@@@&&&                                ((((((((((((((((((((((((((((((((((((((@@\n @@@@&///                                (((((((((((((((((((((((((((((((((((&&&@@\n @@@@&,,,                                   ((((((((((((((((((((((((((((((((&&&@@\n @@@@&                                      ((((((((((((((((((((((((((((((((&%%@@\n @@@&&               @@@@@@@@,,...          ((((((((((@@@@@@@(((((((((((((((&%%@@\n @@@&&             @@        @@...            (((((@@@(((((((@@@((((((((((((%%%@@\n &&&((                                        (((((((((((((((((((((((((((((((((@@\n &&&..                              @@@@@@@@@@   ((((((((((((((((((((((((((((((&&\n &&&..                              ...@@@@@..     ((((((((((((((((((((((((((((@@\n &&&..                                 ,,,,,            (((((((((((((((((((((((@@\n &&&,,...                              .....               ((((((((((((((((((((@@\n &&&&&,,,                         .....@@@@@.....               ((((((((((((&&&@@\n @@@&&@@@..             //%%%.....@@@@@&&&&&@@@@@.....%%///          (((((&&#%%@@\n @@@@@&&&@@...            ,,,@@@@@&&&&&%%%%%&&&&&@@@@@,,             ..@&&%%(((@@\n @@@@@@@@**&&&,,...       ...,,@@@%%%%%##%%%%%%%%@@,,,..          ...&&(%#(((((@@\n @@@@@@@@  ,,,&&,,,..        ..@@@%%%%%((%%%%%%%%@@...       ...,,&&&##%(((((((@@\n @@@@@@@@@@@@@%%&&&%%,,,.....  ...@@   %%%%%  @@@..        ,,%%%&&%%%@@@@@@@@@@@@\n @@@@@@@@@@@@@@@%%%##&&&%%,,,,,...  @@@@@@@@@@.  ..**,((%%%&&%##%%%@@@@@@@@@@@@@@\n @@@@@@@@@@@@@@@@@@@@%%%##&&&&&%%%,,,,,,,,,,,,,,,%%&&&&&%##%%%@@@@@@@@@@@@@@@@@@@\n @@@@@@@@@@@@@@@@@@@@@@@@@&&&%%###&&&&&&&&&&&&&&&##%%%&&&@@@@@@@@@@@@@@@@@@@@@@@@\n"
+         break
+         ;;
+       "Crontab")
+        cronTab
+         break
+         ;;
+
+    esac
+  done
+
+}
+
 networkSelect
 sleep 0.5
 echo 'Enter your pool name (example: chester-validator)'
@@ -52,22 +93,18 @@ echo "Enter account name (example: chester)"
 read a
 sleep 0.5
 
-cat >> $homedir/ping.sh << EOF
-#!/bin/bash
-export NEAR_ENV=$NETWORK
-near call $p$POOL ping '{}' --accountId $a$ACCOUNT --gas=300000000000000
-EOF
+if [[ "$NETWORK" == "guildnet" ]]
+then
+  echo 'You have chosen Gildnet. Want to use croncat or crontab?'
+  sleep 0.5
+  cronSelect
+else
+  cronTab
+fi
 
-chmod +x $homedir/ping.sh
 
-# Adding to Cron
-crontab -l > mycron
-# echo new cron into cron file
-echo "0 */1 * * * $homedir/ping.sh >> $homedir/near.log 2>&1" >> mycron
-# install new cron file
-crontab mycron
-rm -f mycron
 
+echo 'Selected network: '$NETWORK
 echo 'Your pool: '$p$POOL
 echo 'Your account: '$a$ACCOUNT
 echo '----------------------------'
